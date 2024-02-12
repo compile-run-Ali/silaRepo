4cc99def6f1806626dea36f948e7fa33bf2da02a
compile-run-ali <aabdullah.bscs20seecs@seecs.edu.pk>
2024-02-12 09:09:07 +0500

    bug fix

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,16 +3,21 @@ import Dashboard from "@/components/Dashboard/Dashboard"
 import BaseLayout from "@/components/BaseLayout/BaseLayout"
 import { useSession } from "next-auth/react"
 import { useRouter } from "next/router"
+import { useEffect } from "react" // Import useEffect
 
 export default function Login(){
     const {data: session} = useSession()
     const router = useRouter()
 
-    if(!session){
-        router.push("/login")
-    }
+    // Use useEffect to perform side effects after rendering
+    useEffect(() => {
+        // Redirect if there is no session
+        if(!session){
+            router.push("/login")
+        }
+    }, [session, router]) // Add session and router to the dependency array
 
-    
+    // Render the component
     return(
         <BaseLayout title={"Dashboard"}>
             <Dashboard />
